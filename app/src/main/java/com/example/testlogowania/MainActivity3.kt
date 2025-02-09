0a420dade0060b8da686e0cd7ff368ec7bdf8887
JakubOczkowicz <oczkowicz.jakub@gmail.com>
2025-02-09 19:46:40 +0100

    Połączenie w jedno Logowanie tylko jendego typu użytkownika

--- app/src/main/java/com/example/testlogowania/MainActivity3.kt
+++ app/src/main/java/com/example/testlogowania/MainActivity3.kt
@@ -1,11 +1,15 @@
 package com.example.testlogowania
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -14,51 +18,21 @@ import org.json.JSONObject
 class MainActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main3)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun onCreatePlanClicked(view: View) {
+        val intent = Intent(this, GenderSelectionAction::class.java)
+        startActivity(intent)
     }
 
-    fun onClickQuery(v: View) {
-
-        val  user = MainActivity.username
-        val  password = MainActivity.password
-        val  query = findViewById<EditText>(R.id.editTextQuery).text.toString()
-
-        //val url = getString(R.string.db_url_xampp)
-        val url = getString(R.string.db_url)
-
-
-        // Post parameters
-        val jsonObject = JSONObject()
-        jsonObject.put("username",user)
-        jsonObject.put("password",password)
-        jsonObject.put("email","")
-        jsonObject.put("query",query)
-
-        Log.d("fun onClickQuery:","jsonObject: $jsonObject")
-
-        // Volley post request with parameters
-        val requestPOST = JsonObjectRequest(Request.Method.POST,url,jsonObject,
-            Response.Listener { response ->
-                // Process the json
-                try {
-
-                    Log.d("fun onClickQuery:","Response: $response")
-                    printResult(response)
-                }catch (e:Exception){
-                    Log.d("fun onClickQuery:","Exception: $e")
-                }
-
-            }, Response.ErrorListener{
-                // Error in request
-                Log.d("fun onClickQuery:","Volley error: $it")
-            })
-
-        VolleySingleton.getInstance(this).addToRequestQueue(requestPOST)
-
+    fun onTimetableCheckClicked(view: View) {
+        val intent = Intent(this, Harmonogram1::class.java)
+        startActivity(intent)
     }
-
-    fun printResult(result : JSONObject) {
-        findViewById<TextView>(R.id.resultTextView).text = result["message"].toString()
-    }
-
 }
